24915115fbefd220c05e7a3bbca56d17a2d22149
Luis <49037033+luislizcano@users.noreply.github.com>
2022-05-20 15:16:13 -0400

    Update functions.py

--- bin/functions.py
+++ bin/functions.py
@@ -270,11 +270,18 @@ def turbidityMask(image,geometry,nir,swir,blue,land):
       'crs': 'EPSG:4326'})
     thr2 = ee.Number(stats3.get('NDSI'))
     
+    ## Define a boxcar or low-pass kernel (Used if want to smooth the image)
+    smooth = ee.Kernel.euclidean(**{
+        'radius': 1, 
+        'units': 'pixels', 
+        'normalize': True
+    })
+    
     ## Apply threshold value and mask possible shallow seagrass patches.
     ndsi_mask = ndsi.gte(thr2).Not()
     final_mask = ee.Image(maskTurbidity).updateMask(ndsi_mask)
     final_mask = final_mask.unmask(0).clip(geometry)
-    final_mask = final_mask.updateMask(land.max()).Not()
+    final_mask = final_mask.updateMask(land.max()).Not().convolve(smooth)
     output = image.updateMask(final_mask)
     
     return output
